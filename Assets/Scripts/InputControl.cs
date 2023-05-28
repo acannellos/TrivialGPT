da8312e4ecc35195f33efedb9eedeeac63eeac26
Alex Cannellos <ajc5645@gmail.com>
2023-05-28 19:53:46 -0400

    fix input field

--- Assets/Scripts/InputControl.cs
+++ Assets/Scripts/InputControl.cs
@@ -7,31 +7,28 @@ public class InputControl : MonoBehaviour
     [SerializeField] private TMP_InputField inputField;
     [SerializeField] private GameObject caret;
     [SerializeField] private LogControl log;
-    private bool canType = true;
+    [SerializeField] private GameData data;
 
     private void Start()
     {
         caret.SetActive(false);
+        inputField.DeactivateInputField();
+        inputField.text = string.Empty;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Equals))
+        if (data.state == GameState.ReadyForInput)
         {
-            canType = !canType;
-            inputField.enabled = canType;
-
-            if (canType)
-            {
-                caret.SetActive(true);
-                inputField.ActivateInputField();
-            }
-            else
-            {
-                caret.SetActive(false);
-                inputField.DeactivateInputField();
-                inputField.text = string.Empty;
-            }
+            inputField.enabled = true;
+            caret.SetActive(true);
+            inputField.ActivateInputField();
+        }
+        else
+        {
+            caret.SetActive(false);
+            inputField.DeactivateInputField();
+            inputField.text = string.Empty;
         }
     }
 
@@ -40,52 +37,9 @@ public class InputControl : MonoBehaviour
         if (inputField.text != string.Empty)
         {
             log.AddLog(s);
+            data.state = GameState.Rolling;
+            log.AddLog("ANSWER");
+            log.AddLog("Ready to roll...");
         }
     }
 }
-
-/*
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
-
-public class InputControl : MonoBehaviour
-{
-    private string input;
-    [SerializeField]
-    private TMP_InputField inputField;
-
-    private void Start()
-    {
-        if (inputField != null)
-        {
-            inputField.interactable = true;
-            inputField.Select();
-        }
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            ReadInput();
-        }
-    }
-
-    public void ReadStringInput(string s)
-    {
-        input = s;
-        Debug.Log(s);
-    }
-
-    private void ReadInput()
-    {
-        if (inputField.text != string.Empty && inputField.isFocused)
-        {
-            ReadStringInput(input);
-            inputField.text = string.Empty;
-        }
-    }
-}
-*/
